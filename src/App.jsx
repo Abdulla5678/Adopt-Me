c00f33b75d5063f77aef6ee6d3033debf6853873
abdullah <abdallaashrf155@gmail.com>
2024-07-17 17:07:17 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+import SearchParams from "./SearchParams";
+
+
+const App = ()=> {
+    return(
+        <div>
+            <h1>Adopt Me!</h1>
+            <SearchParams />
+        </div>
+    )
+}
+
+const container = document.getElementById('root');
+const root =createRoot(container);
+root.render(<App/>)
+
+export default App;
---
+++ src/Results.jsx
@@ -0,0 +1,24 @@
+import Pet from "./Pet";
+
+const Results = ({pets})=>{
+    return (
+        <div className="search">
+            {!pets.length?(
+                <h1>No pets found</h1>
+            ): (
+                pets.map((pet)=>(
+                    <Pet 
+                        animal={pet.animal} 
+                        breed={pet.breed} 
+                        name={pet.name}
+                        location={`${pet.city} ,${pet.state}`}
+                        images={pet.images}
+                        key={pet.id}
+                    />
+                ))
+            )}
+        </div>
+    )
+}
+
+export default Results;
